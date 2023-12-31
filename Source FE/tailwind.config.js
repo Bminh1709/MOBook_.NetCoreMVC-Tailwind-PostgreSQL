62ef0d0bd72e3b41509c37b2316ee720cda6ecfe
github_username <you@example.com>
2023-12-31 23:27:55 +0700

    Add book and detail pages

--- Source FE/tailwind.config.js
+++ Source FE/tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
       colors: {
         PalePurple: "#F1EEFF",
         DarkPurple: "#6C5DD4",
+        LightGrey: "#F6F6F6",
         PaleGrey: "#C8C8C8",
         DarkGrey: "#6C6C6C",
         DarkYellow: "#FCD300",
